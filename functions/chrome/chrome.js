75007e0b5d08c9485f6c1569ac774ad63fe9dc0e
Johan Burmester <johan.burmester@hemnet.se>
2020-11-03 13:43:11 +0100

    Update chrome.js

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -2,53 +2,49 @@ const chromium = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context) => {
-  let theTitle = null
-  let browser = null
-  console.log('spawning chrome headless')
-  try {
-    const executablePath = await chromium.executablePath
 
-    // setup
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: executablePath,
-      headless: chromium.headless,
-    })
+    // let saljkollen = JSON.parse(event.body).url;
+    const saljkollen = "https://www.hemnet.se/saljkollen/17078586/7a0ea7ad148accd3c5cbfbe91af48c4548da1e9e6cdf74a9cbd463226e87579c?asdfasdfas"
+    const url = saljkollen.split("?")[0]+'?role=admin';
+ 
+    const browser = await chromium.puppeteer.launch({
+        executablePath: await chromium.executablePath,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless
+    });
 
-    // Do stuff with headless chrome
     const page = await browser.newPage()
-    const targetUrl = 'https://davidwells.io'
-
-    // Goto page and then do stuff
-    await page.goto(targetUrl, {
-      waitUntil: ["domcontentloaded", "networkidle0"]
-    })
-
-    await page.waitForSelector('#phenomic')
-
-    theTitle = await page.title();
-
-    console.log('done on page', theTitle)
-
-  } catch (error) {
-    console.log('error', error)
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error
-      })
+    
+    try{
+        await page.goto(url)
+        await page.setViewport({
+            width: 1200,
+            height: 800
+        });
+        await page.waitForTimeout(500)
+        //const visits = await page.$eval('.sellers-dashboard__primary-number', el => el.innerText)
+        const raketen = await page.$eval('.sellers-dashboard__secondary-number', el => el.innerText)
+        await browser.close()
+        return {
+            statusCode: 200,
+            body: "Raketen: " + raketen
+          }
+    } catch(err) {
+        try{
+            const visits = await page.$eval('.seller-listing__times-viewed-counter', el => el.innerText)
+            await browser.close()
+            return {
+                statusCode: 200,
+                body: "Visits: " + visits
+              }
+        }catch (err2){
+            console.log("error",err, err2)
+            await browser.close()
+            return {
+                statusCode: 500
+              }
+        }
+        
     }
-  } finally {
-    // close browser
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      title: theTitle,
-    })
-  }
 }
