de3a7b0d571bad65545268e5e54d27ffc6a315e8
Johan Burmester <johan.burmester@hemnet.se>
2020-12-11 11:38:18 +0100

    remove pup

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -1,5 +1,4 @@
 const chromium = require('chrome-aws-lambda')
-const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context, callback) => {
     // let url = JSON.parse(event.body).url;
@@ -10,7 +9,7 @@ exports.handler = async (event, context, callback) => {
       const executablePath = await chromium.executablePath
   
       // setup
-      browser = await puppeteer.launch({
+      browser = await chromium.puppeteer.launch({
         args: chromium.args,
         executablePath: executablePath,
         headless: chromium.headless,
